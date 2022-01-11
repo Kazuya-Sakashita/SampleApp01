c2efd0c94ce1b62673d8e3e72da369d3572411e9
Kazuya-Sakashita <kz0508@gmail.com>
2022-01-11 09:07:54 +0900

    destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root 'posts#index'
-  resources :posts do
-    resources :comments, :only => [:create, :destroy]
-  end
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
