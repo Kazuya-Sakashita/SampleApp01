d4eaede43ef51adc6ca80c4818f67c38ed95b516
Kazuya-Sakashita <kz0508@gmail.com>
2022-01-16 14:17:51 +0900

    tailwind css 導入

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.create!(
+  [
+    {title: '案内',content: '病院へは、どう行けば良いですか？',author: 'test name1'},
+    {title: '案内',content: '交番へは、どう行けば良いですか？',author: 'test name2'},
+    {title: '案内',content: 'スーパーへは、どう行けば良いですか？',author: 'test name3'},
+    {title: '案内',content: '駅へは、どう行けば良いですか？',author: 'test name4'},
+    {title: '案内',content: 'バス停へは、どう行けば良いですか？',author: 'test name5'},
+    {title: '案内',content: '病院へは、どう行けば良いですか？',author: 'test name1'},
+    {title: '案内',content: '病院へは、どう行けば良いですか？',author: 'test name1'},
+    {title: '案内',content: '病院へは、どう行けば良いですか？',author: 'test name1'},
+    {title: '案内',content: '病院へは、どう行けば良いですか？',author: 'test name1'},
+    {title: '案内',content: '病院へは、どう行けば良いですか？',author: 'test name1'},
+    {title: '案内',content: '病院へは、どう行けば良いですか？',author: 'test name1'},
+    {title: '案内',content: '病院へは、どう行けば良いですか？',author: 'test name1'},
+    {title: '案内',content: '病院へは、どう行けば良いですか？',author: 'test name1'},
+    {title: '案内',content: '病院へは、どう行けば良いですか？',author: 'test name1'},
+  ]
+)
