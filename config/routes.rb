915635c674bf274fa1e3e9ec492a9e9afa571b3d
Kazuya-Sakashita <kz0508@gmail.com>
2022-01-28 06:46:36 +0900

    devise install /homes index追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   }
   root 'posts#index'
   resources :posts
+  resources :homes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
