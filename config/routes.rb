d6a8af18496d3eb89e87356d3d6f9f4b7440f86e
Kazuya-Sakashita <kz0508@gmail.com>
2022-02-05 07:28:30 +0900

    mypage追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   root 'posts#index'
   resources :posts
   resources :homes
+  resources :mypages
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |n|
+  # 一般ユーザー
+  User.create!(
+    email: "test+#{n + 1}@test.com",
+    password: "password",
+    password_confirmation: "password"
+  )
+end
