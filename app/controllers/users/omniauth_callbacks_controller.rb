1bf916ff21bbf603e3c16ea541b0fe8ace12187d
Kazuya-Sakashita <kz0508@gmail.com>
2022-02-18 07:40:56 +0900

    OmniauthCallbacksController修正

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -20,6 +20,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       session["devise.#{provider}_data"] = request.env['omniauth.auth'].except('extra') # Removing extra as it can overflow some session stores
       redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
-  end
 
+    def failure
+      redirect_to root_path
+    end
+  end
 end
