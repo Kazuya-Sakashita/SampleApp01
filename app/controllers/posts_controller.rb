669d439835a92b6df94aa373742179b5050c56eb
Kazuya-Sakashita <kz0508@gmail.com>
2022-04-03 07:13:22 +0900

    shareの投稿のみposts#indexで表示する

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
   def index
     @q = Post.ransack(params[:q])
     @posts = @q.result(distinct: true).page(params[:page]).per(10)
+    @posts = @posts.where(post_published_state:"share")
   end
 
 
